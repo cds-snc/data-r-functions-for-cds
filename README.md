1ed2f12392d1a6af13a3dbeadec54bb7912601f9
Colin Douglas <colin.douglas@cds-snc.ca>
2026-02-04 11:57:12 -0400

    feat: better manual matching flows (#5)
    * chore: bump version number
    
    * chore: update readme
    
    * fix: add additional identifiers
    
    * feat: make manual matches easier
    
    * feat: fix common typos automatically
    
    * chore: update version for release
    
    * fix: remove delisted suggest
    
    * fix: update license file to align with standards

--- README.md
+++ README.md
@@ -8,6 +8,9 @@ analyses easier. It includes:
 * a list of commonly used colors (`cds::colors`)
 * a list of commonly used colours (`cds::colours`)
 
+<!-- badges: start -->
+  [![R-CMD-check](https://github.com/cds-snc/data-r-functions-for-cds/actions/workflows/R-CMD-check.yaml/badge.svg)](https://github.com/cds-snc/data-r-functions-for-cds/actions/workflows/R-CMD-check.yaml)
+<!-- badges: end -->
 
 ## Installation
 
@@ -52,16 +55,34 @@ into the [official names](https://github.com/cds-snc/gc-organisations/), using
 name standardization, fuzzy joins and some ugly heuristics.
 
 ```
-> resolve_name(c("IRCC", "ESDC", "Natural Resources", "Department of Unicorns"))
+> cds::resolve_name(c(
+    "IRCC", 
+    "ESDC", 
+    "Natural Resources", 
+    "tpsgc-pwgsc.gc.ca", 
+    "Department of Unicorns"
+    ))
 
 [1] "Immigration, Refugees and Citizenship Canada" "Employment and Social Development Canada"    
-[3] "Natural Resources Canada"                     NA        
-
+[3] "Natural Resources Canada"                     "Public Services and Procurement Canada"      
+[5] NA                                            
 Warning message:
-  1 names couldn't be matched - returning NA for:
+  1 names couldn't be matched:
    "Department of Unicorns"
 
 ```
+
+### Requesting Additional Matches
+Some matches cannot be made programmatically, and have to be entered manually.
+The CSV file [`manual-matches.csv`](https://github.com/cds-snc/data-r-functions-for-cds/blob/main/data-raw/organisation-resolution/data/manual-matches.csv) contains matches that aren't necessarily intuitive.
+Each line in the CSV has the following fields:
+
+  * `identifier` is the string you want to match. It is eventually normalized and not case sensitive
+  * `resolves_to` is a unique identifier from the [`cds-snc/gc-organisations`](https://www.github.com/cds-snc/gc-organisations) repository
+    * Use "001I9000005Ho3zIAC" for departments not listed in the gc-organisations repo
+  * `source` is where you first found this acronym. It is optional but helpful.
+
+Pull requests are always welcome, generally, but specifically for this file.
 
 ## Package Tests
 
@@ -74,10 +95,10 @@ them, clone this repository, open the project in R Studio, and ensure that the
 
 ℹ Testing cds
 ✔ | F W  S  OK | Context
-✔ |          8 | normalize_name                                                   
-✔ |          4 | org_resolver                                                     
-✔ |         17 | resolve_name                                                     
+✔ |          8 | normalize_name                                                              
+✔ |          4 | org_resolver                                                                
+✔ |         21 | resolve_name                                                                
 
-══ Results ═══════════════════════════════════════════════════════════════════════
-[ FAIL 0 | WARN 0 | SKIP 0 | PASS 29 ]
+══ Results ══════════════════════════════════════════════════════════════════════════════════
+[ FAIL 0 | WARN 0 | SKIP 0 | PASS 33 ]
 ```
--- data-raw/organisation-resolution/04_set-identifiers-manual.R
+++ data-raw/organisation-resolution/04_set-identifiers-manual.R
@@ -4,192 +4,39 @@
 # * Common misspellings that are too distant for fuzzy matching
 # * True mysteries where it just needs to work (e.g., 'treasy' for TBS??)
 
-# In this script, we just manually set them, without much worry about whether
-# it will be reproducible.
+# In this script, we pull in a CSV of manual matches we want to make
+
+# TO ADD TO THE CSV
+# 1. Open the file `data-raw/organisation-resolution/data/manual-matches.csv` in the package directory
+# 2. Add additional rows at the bottom.
+#   - The first column `identifier` is what you want to match - it is normalized, so it is NOT case sensitive
+#   - The second column `resolves_to` is a unique identifier from the `cds-snc/gc-organisations` repository
+#     - Use "001I9000005Ho3zIAC" for departments not listed in the gc-organisations repo
+#   - The third column `source` is where you first found this acronym
+# 3. Run the script `data-raw/organisation-resolution/00_run-all.R` script to create the final table
 
 
-missing_identifier <- "001I9000005Ho3zIAC" # "Department not listed"
-
-# GC Forms ----------------------------------------------------------------
-
-# From GC Forms Demos table
-leftovers_gcforms_demos <- tribble(
-  ~ identifier, ~ resolves_to,
-  "AAFC", "001I9000004pwojIAA",
-  "CATSA", "001I90000050b2KIAQ",
-  "CBSA", "001I9000004Vt5XIAS",
-  "CIPO", "001I90000050aK9IAI",
-  "CRA", "001I9000004Vt5ZIAS",
-  "CRTC", "001I9000004Vt5hIAC",
-  "ECCC", "001I9000004Vt5pIAC",
-  "ESDC", "001I9000004Vt5oIAC",
-  "FCAC", "001I900000503tyIAA",
-  "GAC", "001I9000004Vt5tIAC",
-  "HC", "001I9000004Vt5uIAC",
-  "IRCC", "001I9000004Vt5wIAC",
-  "ISC", "001I9000005HqRoIAK",
-  "ISED", "001I9000004Vt5zIAC",
-  "LAC", "001I9000004Vt60IAC",
-  "NRCan", "001I9000004Vt63IAC",
-  "OSB", missing_identifier,
-  "PBC", "001I900000504ZTIAY",
-  "PC", "001I9000004Vt69IAC",
-  "PHAC", "001I9000004Vt6AIAS",
-  "PS", "001I9000004Vt6CIAS",
-  "PSPC", "001I9000004Vt6EIAS",
-  "RCMP", "001I9000004Vt6GIAS",
-  "StatCan", "001I9000004Vt6IIAS",
-  "TBS", "001I9000004Vt6KIAS",
-  "TC", "001I9000004Vt6JIAS",
-  "VAC", "001I9000004Vt6LIAS"
-) %>%
-  mutate(
-    source = "Airtable/GC Forms",
-    identifier = normalize_name(identifier)
-  )
+# FOR EXAMPLE:
+# Consider the top line of the CSV:
+# "identifier","resolves_to","source"
+# "AAFC","001I9000004pwojIAA","Airtable/GC Forms"
 
 
-# From the organisations assigned as owners of certain Forms
-leftovers_gcforms_forms <- tribble(
-  ~ identifier, ~ resolves_to,
-  "Canadian Digital Service / Service numérique canadien", "001I9000004Vt5oIAC",
-  "Copyright Board Canada / Commission du droit d'auteur Canada", "001I900000503tPIAQ",
-  "National Defence / Défense nationale", "001I9000004Vt61IAC",
-  "Veterans Affairs Canada / Anciens Combattants Canada", "001I9000004Vt6LIAS",
-  "Superintendent of Financial Institutions Canada (Office of the) / Bureau du surintendant des institutions financières Canada", "001I9000005DXSKIA4",
-  "Refugees and Citizenship Canada / Immigration", "001I9000004Vt5wIAC",
-  "Science and Economic Development Canada / Innovation", "001I9000004Vt5zIAC",
-  "Innovation, Science and Economic Development Canada", "001I9000004Vt5zIAC",
-  "Innovation, Science, and Economic Development Canada", "001I9000004Vt5zIAC",
-  '"Innovation, Science and Economic Development Canada"', "001I9000004Vt5zIAC",
-  '"Innovation, Science, and Economic Development Canada"', "001I9000004Vt5zIAC",
-  "National Battlefields Commission / Commission des champs de bataille nationaux", "001I9000005X2DjIAK",
-  "Health Canada / Santé Canada,Public Health Agency of Canada / Agence de la santé publique du Canada", "001I9000004Vt5uIAC",
-  "Office of the Federal Ombudsperson for Victims of Crime", missing_identifier,
-  "Taxpayers' Ombudsperson (Office of the) / Bureau de l'ombudsman des contribuables", "001OO00000Ct4gqYAB",
-  "treasy", "001I9000004Vt6KIAS",
-  "Canadian Forces Morale and Welfare Services", missing_identifier,
-  "Competition Bureau Canada", missing_identifier,
-  "Canada Border Services Agency / Agence des services frontaliers du Canada,Treasury Board of Canada Secretariat / Secrétariat du Conseil du Trésor du Canada", "001I9000004Vt5XIAS",
-) %>%
-  mutate(
-    source = "Airtable/GC Forms",
-    identifier = normalize_name(identifier)
-  )
+# This identifier was first observed in an Airtable published by GC Forms
+# The line indications that the abbreviation "AAFC" matches the organisation ID "001I9000004pwojIAA"
+# Looking this ID up in the `cds-snc/gc-organisations` repo shows that this ID means "Agriculture and Agri Food Canada"
 
-orgs <- bind_rows(orgs, leftovers_gcforms_demos, leftovers_gcforms_forms)
+manual_matches <- read_csv(
+  "data-raw/organisation-resolution/data/manual-matches.csv",
+  col_names = TRUE,
+  cols(
+    identifier = col_character(),
+    resolves_to = col_character(),
+    source = col_character()
+  )) %>%
+  mutate(identifier = normalize_name(identifier)) %>%
+  replace_na(list(identifier = "001I9000005Ho3zIAC"))  # Replace missing identifiers with "Department not listed"
 
-
-# GC Design System --------------------------------------------------------
-
-# From GCDS's CRM
-leftovers_gcds <- tribble(
-  ~ identifier, ~ resolves_to,
-  "Ship-source Oil Pollution Fund", missing_identifier,
-  "City of Vancouver", missing_identifier,
-  "\"Innovation, Science and Economic Development Canada\"", "001I9000004Vt5zIAC",
-  "\"Immigration, Refugees and Citizenship Canada\"", "001I9000004Vt5wIAC",
-  "External to government", missing_identifier,
-  "Windsor-Detroit Bridge Authority,External to government", missing_identifier,
-  "Taxpayers' Ombudsperson (Office of the)", "001OO00000Ct4gqYAB",
-  "Canadian Digital Service,Employment and Social Development Canada", "001I9000004Vt5oIAC",
-  "Canada Revenue Agency,External to government", "001I9000004Vt5ZIAS",
-  "Employment and Social Development Canada,Canadian Digital Service", "001I9000004Vt5oIAC",
-  "External to government", missing_identifier,
-  "Canadian Digital Service", "001I9000004Vt5oIAC"
-) %>%
-  mutate(
-    source = "Airtable/GC Design System",
-    identifier = normalize_name(identifier)
-  )
-
-orgs <- bind_rows(orgs, leftovers_gcds)
-
-
-# Enterprise --------------------------------------------------------------
-
-leftovers_enterprise <- tribble(
-  ~ identifier, ~ resolves_to,
-  "CAS", "001I9000004Vt5kIAC",
-  "ACOA", "001I9000004Vt5WIAS",
-  "CAF", "001I9000004Vt61IAC",
-  "CCCS", "001I9000004zLveIAE", # Cyber Security
-  "CCOHS", "001I900000503ncIAA",
-  "CEDQ", "001I9000004Vt5YIAS",
-  "CER", "001I9000005tFjAIAU",
-  "CFIA", "001I9000004Vt5bIAC",
-  "CHRC", "001I9000004Vt5eIAC",
-  "CICS", "001OO00000IclDKYAZ",
-  "CIHR", "001I900000503mHIAQ",
-  "CIRNAC", "001I9000004Vt5xIAC",
-  "CJC", missing_identifier,
-  "Competition Tribunal", missing_identifier,
-  "Copyright Board", "001I900000503tPIAQ",
-  "CSC, PBC", "001I900000503m9IAA",
-  "CSPS", "001I9000004Vt5aIAC",
-  "EDC", "001I900000503nDIAQ",
-  "EI BOA", missing_identifier,
-  "FBCL", "001I9000004Vt6JIAS",
-  "FCAS", "001I900000503tyIAA",
-  "Federal Court", missing_identifier,
-  "Federal Court of Appeal", missing_identifier,
-  "FINTRAC", "001I9000004Vt5rIAC",
-  "FPSLREB", missing_identifier,
-  "IAAC", "001I9000005DbFCIA0",
-  "IDRC", missing_identifier,
-  "IOGC", missing_identifier,
-  "Justice", "001I9000004Vt5mIAC",
-  "NCC Ombudsman", "001I9000004Vt5mIAC",
-  "NSERC", "001I9000004Vt64IAC",
-  "Office of the Commissioner of Lobbying", "001I9000004Vt65IAC",
-  "Office of the Public Sector Integrity Commissioner", "001OO00000HzMoJYAV",
-  "OPC", "001I900000504b0IAA",
-  "OSFI", "001I9000005DXSKIA4",
-  "PCA", "001I9000004Vt67IAC",
-  "PrairiesCan", "001I9000004Vt68IAC",
-  "PrairiesCan and PacifiCan", "001I9000004Vt68IAC",
-  "RMC", "001I9000004Vt61IAC",
-  "Specific Claims Tribunal", missing_identifier,
-  "SSC", "001I9000004Vt6HIAS",
-  "SSHRC", "001I900000503tgIAA",
-  "Tax Court of Canada", missing_identifier,
-  "Veterans Ombud", "001I900000505KfIAI",
-  "WGEC", "001I9000004zOAzIAM",
-  "OSGG", "001OO00000AhiwAYAR"
-) %>%
-  mutate(
-    source = "Airtable/Enterprise",
-    identifier = normalize_name(identifier)
-  )
-
-
-
-# Left from GEDS dump -----------------------------------------------------
-
-leftovers_geds_names <- tribble(
-  ~ identifier, ~ resolves_to,
-  "Atomic Energy of Canada Limited", "001I900000505PuIAI",
-  "Canada Agricultural Review Tribunal", missing_identifier,
-  "Canada Council for the Arts", missing_identifier,
-  "Housing, Infrastructure and Communities Canada", "001I9000004Vt5yIAC",
-  "Innovation, Science and Economic Development Canada - Innovation, Sciences et Développement économique Canada", "001I9000004Vt5zIAC",
-  "National Arts Centre", missing_identifier,
-  "Office of the Procurement Ombud", "001I90000050aLkIAI",
-  "The Federal Bridge Corporation Limited", missing_identifier,
-) %>%
-  mutate(
-    source = "GEDS Extract/Org Names",
-    identifier = normalize_name(identifier)
-  )
-
-orgs <- bind_rows(
-  orgs,
-  leftovers_gcforms_demos,
-  leftovers_gcforms_forms,
-  leftovers_gcds,
-  leftovers_enterprise,
-  leftovers_geds_names,
-  missing_domains
-) %>%
+orgs <- bind_rows(orgs, manual_matches) %>%
   distinct(identifier, .keep_all = TRUE)
 
